f72babaa447980d86fdc913b9f77d57e6e9cf766
Fawzi E. Abdulfattah <iifawzie@gmail.com>
2020-06-23 00:46:15 +0200

    Edit the name of the css file

--- dist/app.js
+++ dist/app.js
@@ -1,2 +1,16 @@
 "use strict";
+var ProjectInput = (function () {
+    function ProjectInput() {
+        this.templateElement = document.getElementById("project-input");
+        this.hostElement = document.getElementById("app");
+        var importedNode = document.importNode(this.templateElement.content, true);
+        this.element = importedNode.firstElementChild;
+        this.attach();
+    }
+    ProjectInput.prototype.attach = function () {
+        this.hostElement.insertAdjacentElement('afterbegin', this.element);
+    };
+    return ProjectInput;
+}());
+var prjInput = new ProjectInput();
 //# sourceMappingURL=app.js.map
